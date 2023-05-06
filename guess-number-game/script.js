2d17b3065bfe25bfae78761219a840580794b025
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-05-06 20:57:45 +0100

    Function returning 1

--- guess-number-game/script.js
+++ guess-number-game/script.js
@@ -1,1 +1,19 @@
-// Your solution goes here 
+function playGuessingGame(numToGuess, totalGuesses = 10) {
+
+  let attempt = 0
+
+  let guess = prompt("Enter a number between 1 and 100.");
+
+  attempt++
+
+  if (guess == numToGuess) {
+    return attempt
+  } 
+}
+
+
+// numToGuess < guess return 
+// numToGuess > guess return 
+
+// guess > 3 return 3
+// != guess > 10 return 0
