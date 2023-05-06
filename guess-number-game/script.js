c9c41ef625cf08e7fbddd86472498ad782d7029e
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-05-06 23:31:51 +0100

    Passing with 8, needs fixing last test

--- guess-number-game/script.js
+++ guess-number-game/script.js
@@ -1,19 +1,30 @@
 function playGuessingGame(numToGuess, totalGuesses = 10) {
+  let attempt = 1;
 
-  let attempt = 0
+  let input = prompt("Enter a number between 1 and 100.");
 
-  let guess = prompt("Enter a number between 1 and 100.");
-
-  attempt++
-
-  if (guess == numToGuess) {
-    return attempt
-  } 
+  while (true) {
+    if (input === "") {
+      input = prompt("Please enter a number.");
+    }
+    if (isNaN(input)) {
+      input = prompt("Please enter a number.");
+    }
+    if (input == numToGuess) {
+      return attempt;
+    } 
+    if (attempt >= totalGuesses || input == null) {
+      return 0;
+    }
+    if (input > numToGuess) {
+      input = prompt(input + " is too large. Guess a smaller number.");
+    } else {
+      input = prompt(input + " is too small. Guess a larger number.");
+    }
+    attempt++;
+  }
 }
 
-
-// numToGuess < guess return 
-// numToGuess > guess return 
-
-// guess > 3 return 3
-// != guess > 10 return 0
+//playGuessingGame(20, 3); 
+//30, 10, "hello", "twenty", 20
+//output: 0 and it should be 3
