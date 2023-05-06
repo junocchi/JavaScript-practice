cd72b1fdd2bab8936ef310fc3a1ec010de7e5425
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-05-06 19:46:13 +0100

    Add first version with console.log

--- divide-array/script.js
+++ divide-array/script.js
@@ -1,7 +1,29 @@
-function divideArray() {
-  
+function divideArray(num) {
+  let evenNums = [];
+  let oddNums = [];
+
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] % 2 === 0) {
+      evenNums.push(num[i]);
+    } else {
+      oddNums.push(num[i]);
+    }
+  }
+
+  // printArray("Even numbers", evenNums)
+  // printArray("Odd numbers", oddNums)
+
+  console.log("Even numbers:"); 
+  console.log(evenNums.sort().join("\n") || "None");
+  console.log("Odd numbers:");
+  console.log(oddNums.sort().join("\n") || "None");
 }
 
+// function printArray(header, array) {
+//   console.log(header + ":");
+//   console.log(array.sort().join("\n") || "None");
+// }
 
 
-divideArray()
+divideArray([4, 2, 9, 1, 8]);
+divideArray([4, 2, 8]);
