9c79823cc41a867191dfe793106409b63b6cdb47
Juliana Nocchi <juliana.nocchi@spendesk.com>
2023-10-22 20:00:53 +0100

    Add script with variables and types

---
+++ 01-fundamentals/script.js
@@ -0,0 +1,37 @@
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is fun!");
+
+console.log(40 + 10 + 23 - 10);
+
+console.log("Jonas");
+let firstName = "Ju";
+
+let country = "UK";
+let continent = "Europe";
+let population = "67 million";
+
+console.log(country + ", " + continent + ", " + population);
+
+let jsIsFun = true;
+console.log(jsIsFun);
+
+console.log(typeof true);
+console.log(typeof jsIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+jsIsFun = "JS is fun!";
+console.log(typeof jsIsFun);
+
+// type undefined
+let year;
+console.log(year); //undefined
+console.log(typeof year); //undefined
+
+year = 1991;
+console.log(typeof year);
+
+//type null
+let picture = null;
+console.log(picture); //null
+console.log(typeof picture); //object
