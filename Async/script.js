f945a73e265a9b51f13865f1e2476851e3541570
Juliana Nocchi <juliana.nocchi@spendesk.com>
2023-11-06 11:52:35 +0000

    Printing numbers not in order

--- Async/script.js
+++ Async/script.js
@@ -1,10 +1,12 @@
 "use strict";
 
+// sync function
 function print1() {
   const number1 = 1;
   console.log(number1);
 }
 
+// print2 is a sync funtion with a blocking call
 function print2() {
   function getNumber2() {
     return 2;
@@ -13,6 +15,7 @@ function print2() {
   console.log(number2);
 }
 
+// asynct function
 function print3() {
   const fs = require("fs");
   fs.readFile("./number3.txt", "utf-8", function (err, number3) {
@@ -20,6 +23,17 @@ function print3() {
   });
 }
 
+// sync function
+function print4() {
+  const number4 = 4;
+  console.log(number4);
+}
+
 print1();
 print2();
 print3();
+print4();
+// 1
+// 2
+// 4
+// 3
