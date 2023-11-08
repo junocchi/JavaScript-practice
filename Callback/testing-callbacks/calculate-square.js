0ad933962e3c125c73f0c94b8bdb29d343766f59
Juliana Nocchi <juliana.nocchi@spendesk.com>
2023-11-08 10:08:32 +0000

    Setup testing

---
+++ Callback/testing-callbacks/calculate-square.js
@@ -0,0 +1,20 @@
+function calculateSquare(number, callback) {
+  setTimeout(function () {
+    if (typeof number !== "number") {
+      callback(new Error("Argument of type number is expected"));
+      return;
+    }
+    const result = number * number;
+    callback(null, result);
+  }, 1000);
+}
+
+calculateSquare(1, function (error, result) {
+  console.log(result);
+  calculateSquare(2, function (error, result) {
+    console.log(result);
+    calculateSquare(3, function (error, result) {
+      console.log(result);
+    });
+  });
+});
