7c73808628e3c467f698b22ca5d90ee431bb1fdc
Juliana Nocchi <juliana.nocchi@spendesk.com>
2023-11-09 09:37:13 +0000

    Add Promise states exercises

--- promises/script.js
+++ promises/script.js
@@ -4,8 +4,19 @@ const myPromise = new Promise(function (resolve, reject) {
   resolve("value");
 });
 console.log(myPromise);
+//Promise { 'value' }
 
 const myPromise2 = new Promise(function (resolve, reject) {
   reject("reason");
 });
 console.log(myPromise2);
+//Promise { <rejected> 'reason' }
+
+const myPromise3 = new Promise(function (resolve, reject) {
+  resolve("value");
+  resolve("value2");
+  reject("reason");
+});
+console.log(myPromise3);
+//Promise { 'value' }
+//The Promise became fulfilled after the first call and never change its data again
