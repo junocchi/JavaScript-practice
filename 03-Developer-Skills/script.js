93773f2ace809203a9f383eab274f369e8123cf6
Juliana Nocchi <juliana.nocchi@spendesk.com>
2023-12-04 16:00:25 +0000

    Modify temperature amplitude to receive two arrays

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,7 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-// TODO
+// PROBLEM 1:
 // Smart meter company. "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
 // 1) understanding the problem
@@ -14,9 +14,35 @@
 // -get the highestTemp
 // -return temp amplitude
 
-const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+// const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 
-const calcTempAmplitude = function (temperatures) {
+// const calcTempAmplitude = function (temperatures) {
+//   let maxTemp = temperatures[0];
+//   let minTemp = temperatures[0];
+
+//   for (let i = 0; i < temperatures.length; ++i) {
+//     const curTemp = temperatures[i];
+//     if (typeof curTemp !== "number") continue;
+
+//     if (curTemp > maxTemp) maxTemp = curTemp;
+//     if (curTemp < minTemp) minTemp = curTemp;
+//   }
+//   console.log(maxTemp, minTemp);
+//   return maxTemp - minTemp; // 17 -6
+// };
+
+// const tempAmplitude = calcTempAmplitude(temperatures);
+// console.log(tempAmplitude); // 23
+
+// PROBLEM 2
+// 1) Function should receive 2 arrays of temperatures
+// -should we implement the functionality twice? Answer: no. Just merge the arrays at the beginning.
+// 2) sub-problem
+// -Merge two arrays
+
+const calcTempAmplitude = function (temp1, temp2) {
+  const temperatures = temp1.concat(temp2);
+
   let maxTemp = temperatures[0];
   let minTemp = temperatures[0];
 
@@ -28,8 +54,11 @@ const calcTempAmplitude = function (temperatures) {
     if (curTemp < minTemp) minTemp = curTemp;
   }
   console.log(maxTemp, minTemp);
-  return maxTemp - minTemp; // 17 -6
+  return maxTemp - minTemp; // 20 -6
 };
 
-const tempAmplitude = calcTempAmplitude(temperatures);
-console.log(tempAmplitude); // 23
+const tempAmplitude = calcTempAmplitude(
+  [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5],
+  [-3, -1, "error", 9, 13, 20, 5],
+);
+console.log(tempAmplitude); // 26
