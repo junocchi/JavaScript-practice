cc5af95c68f26d4c8e82f07197300cbfc50b3a36
Juliana Nocchi <juliana.nocchi@spendesk.com>
2023-12-04 15:40:23 +0000

    Calculate temperature amplitude

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -2,7 +2,34 @@
 "use strict";
 
 // TODO
-const x = "23";
+// Smart meter company. "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
-const calcAge = (birthYear) => 2023 - birthYear;
-console.log(calcAge(1983));
+// 1) understanding the problem
+// -whats temperature amplitude? Answer: the difference between the highest and the lowest temperature
+// -whats a sensor error? And what to do? Answer: it returns 'error'
+
+// 2) Breaking up into sub-problems
+// -remove not valid values
+// -get the lowestTemp
+// -get the highestTemp
+// -return temp amplitude
+
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitude = function (temperatures) {
+  let maxTemp = temperatures[0];
+  let minTemp = temperatures[0];
+
+  for (let i = 0; i < temperatures.length; ++i) {
+    const curTemp = temperatures[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > maxTemp) maxTemp = curTemp;
+    if (curTemp < minTemp) minTemp = curTemp;
+  }
+  console.log(maxTemp, minTemp);
+  return maxTemp - minTemp; // 17 -6
+};
+
+const tempAmplitude = calcTempAmplitude(temperatures);
+console.log(tempAmplitude); // 23
